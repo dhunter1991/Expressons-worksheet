fa2f0f9cbcf40e0e1b1407455d2ba36f6cf8822c
dhunter1991 <hunterdylan882@gmail.com>
2015-10-04 18:22:45 -0400

    help

--- js/script.js
+++ js/script.js
@@ -1,13 +1,7 @@
 // Expressions worksheet Dylan Hunter
 
-var groceryCost = [prompt ("week one grocery cost"), prompt("week two grocery cost"), prompt("week three grocery cost"), prompt("week four grocery cost"),
-prompt("week five grocery cost")];
-var totalCost = groceryCost[0]  + groceryCost[1]  + groceryCost[2] + groceryCost[3]
-    + groceryCost[4];
 
-var average = totalCost/5;
 
-alert( "you have spent " + totalCost, " on groceries over 5 weeks.", "\nyour avergage grocery cost is " + average);
 
 
 
